be65327b79deaba5cb7df07a83505884d245a08a
Karan Kumar <karankumar072508@gmail.com>
2023-02-28 03:12:44 +0530

    adding methods

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// All variables
+const activeAlarm = document.getElementsByClassName('alarm-active');
+const alarmText = document.getElementById('alarm-data');
+
+
+
 // function which get time
 const getTime = ({hours, minutes, seconds, zone}) => {
     if(minutes/10 < 1) {
@@ -14,7 +20,7 @@ const getTime = ({hours, minutes, seconds, zone}) => {
 const renderTime = () => {
     var currentTime = document.getElementById('current-time');
 
-    // using Date calss mto get the dta of date
+    // using Date class mto get the dta of date
     const currentDate = new Date();
     let hours = currentDate.getHours();
     let minutes = currentDate.getMinutes();
@@ -30,3 +36,35 @@ const renderTime = () => {
 
 //updating time every second
 setInterval(renderTime, 1000);
+
+
+// value of current alarm
+let alarmString = null;
+
+// Handling Alarm
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const { hour, min, sec, zone } = document.forms[0];
+    alarmString = getTime({
+        hours: hour.value,
+        minutes: min.value,
+        seconds: sec.value,
+        zone: zone.value,
+        
+    });
+
+    //Reseting after submit
+    document.forms[0].reset();
+
+    // Hide create alarm
+  createAlarm.style.display = "none";
+
+  // show active alarm with text
+  activeAlarm.style.display = "block";
+
+    alarmText.innerHTML = alarmText(alarmString);
+};
+
+
+// adding submit to the forms
+// document.forms[0].addEventListener("submit", handleSubmit);
